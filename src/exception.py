c86fa4649200da163322017eeac1f22182d30d06
jimmymuthoni <jimmymuthoni26@gmail.com>
2024-11-19 15:51:46 +0300

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def error_message_details(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -16,4 +17,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
